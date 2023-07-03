3b0b82a48a3eee2a9b8e23a13e782731603e10c5
ramon <reimon1972@gmail.com>
2023-07-03 16:39:30 +0800

    add redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
 import {
-  createBrowserRouter,
   RouterProvider,
-  Navigate,
 } from 'react-router-dom'
-import MainLayout from './layouts/MainLayout.tsx'
-import Home from './pages/home/Home.tsx'
-import Product from './pages/product/Product.tsx'
-import PageNotFoundView from './error/PageNotFoundView.tsx'
+import { Provider } from 'react-redux'
+import router from './routers/routers.tsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      { path: '*', element: <Navigate to='/404' /> },
-      { path: '/', element: <Home /> },
-      { path: '404', element: <PageNotFoundView /> },
-      { path: '/product/:id', element: <Product /> },
-    ],
-  },
-  {
-    path: '/product/:id',
-    element: <Product />,
-  },
-])
+import { store } from './store/store.ts'
+
 function AppRouter() {
-  return <RouterProvider router={router} />
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  )
 }
 
 export default AppRouter
---
+++ src/routers/routers.tsx
@@ -0,0 +1,27 @@
+import {
+  createBrowserRouter,
+  Navigate,
+} from 'react-router-dom'
+import MainLayout from '../layouts/MainLayout.tsx'
+import Home from '../pages/home/Home.tsx'
+import Product from '../pages/product/Product.tsx'
+import PageNotFoundView from '../error/PageNotFoundView.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { path: '*', element: <Navigate to='/404' /> },
+      { path: '/', element: <Home /> },
+      { path: '404', element: <PageNotFoundView /> },
+      { path: '/product/:id', element: <Product /> },
+    ],
+  },
+  {
+    path: '/product/:id',
+    element: <Product />,
+  },
+])
+
+export default router
